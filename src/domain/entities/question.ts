9eaa3f659d56ca0e6085382ab4ec828d1793a266
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-04-11 17:46:09 -0300

    Fundamentos de DDD - Value Object de slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,20 +1,24 @@
 import { randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
   title: string
   content: string
+  slug: Slug
   authorId: string
 }
 
 export class Question {
   public id: string
   public title: string
+  public slug: Slug
   public content: string
   public authorId: string
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title
     this.content = props.content
+    this.slug = props.slug
     this.authorId = props.authorId
     this.id = id ?? randomUUID()
   }
---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Receives a string and normalize it as a slug.
+   *
+   * Example: "An example title" => "an-example-title"
+   *
+   * @param text {string}
+   */
+  static createFromText(text: string): Slug {
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
+
+    return new Slug(slugText)
+  }
+}
