acd045b512044aa01b391b689b5642d809f7ce8b
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-05-11 12:09:31 -0300

    Functional Error Handling - Testando classes de erro

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,22 +1,38 @@
 // Error
-export class Left<L> {
+export class Left<L, R> {
   readonly value: L
 
   constructor(value: L) {
     this.value = value
   }
+
+  isRight(): this is Right<L, R> {
+    return false
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
+  }
 }
 
 // Success
-export class Right<R> {
+export class Right<L, R> {
   readonly value: R
 
   constructor(value: R) {
     this.value = value
   }
+
+  isRight(): this is Right<L, R> {
+    return true
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false
+  }
 }
 
-export type Either<L, R> = Left<L> | Right<R>
+export type Either<L, R> = Left<L, R> | Right<L, R>
 
 export const left = <L, R>(value: L): Either<L, R> => {
   return new Left(value)
